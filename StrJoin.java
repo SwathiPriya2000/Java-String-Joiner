6b4c621e81e862ca0f816b0f9f8e880db622f798
SwathiPriya2000 <63891711+SwathiPriya2000@users.noreply.github.com>
2020-09-27 23:03:27 +0530

    Add files via upload

---
+++ StrJoin.java
@@ -0,0 +1,18 @@
+import java.util.StringJoiner;  
+public class StrJoin {  
+    public static void main(String[] args) {  
+    	// Passing Hyphen(-) as delimiter
+        StringJoiner mystring = new StringJoiner("-");    
+          
+        // Joining multiple strings by using add() method  
+        mystring.add("Logan");  
+        mystring.add("Magneto");  
+        mystring.add("Rogue");  
+        mystring.add("Storm");  
+                  
+        // Displaying the output String
+        System.out.println(mystring);  
+    }  
+}
+
+
